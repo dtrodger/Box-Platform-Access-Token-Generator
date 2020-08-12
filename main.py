ee49f3b6572558b47ef06746c1261b0c4de542b4
boxdave <drodgers@box.com>
2020-08-12 13:41:13 -0500

    adds box client getter

--- main.py
+++ main.py
@@ -11,14 +11,18 @@ import boxsdk
 BOX_JWT_KEYS_FILE_PATH = os.path.join(
     os.path.dirname(__file__), "box_jwt_keys.yml"
 )
+box_jwt_keys = None
+box_client = None
 
 
 def get_box_jwt_keys():
     """
     Loads Box Platform JWT keys into a global variable.
     """
-    with open(BOX_JWT_KEYS_FILE_PATH, "r") as fh:
-        box_jwt_keys = yaml.load(fh, Loader=yaml.FullLoader)
+    global box_jwt_keys
+    if not box_jwt_keys:
+        with open(BOX_JWT_KEYS_FILE_PATH, "r") as fh:
+            box_jwt_keys = yaml.load(fh, Loader=yaml.FullLoader)
 
     return box_jwt_keys
 
@@ -32,6 +36,18 @@ def get_box_auth():
     box_auth.authenticate_instance()
 
     return box_auth
+
+
+def get_box_client():
+    """
+    Sets up a Box Platform API client as a global variable.
+    """
+    global box_client
+    if not box_client:
+        box_auth = get_box_auth()
+        box_client = boxsdk.Client(box_auth)
+
+    return box_client
 
 
 if __name__ == "__main__":
